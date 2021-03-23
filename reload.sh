0d8f0f9dd3d4389761ccb485fe84c8a502ea0348
Cory Siebler <cory.siebler@er2.com>
2021-03-22 22:08:43 -0700

    Removed git alias, reduntant with oh-my-zsh

--- reload.sh
+++ reload.sh
@@ -1,9 +1,8 @@
 # Move the individual alias files to the user's directory
 cp -f .docker_aliases ~/
-cp -f .git_aliases ~/
 
 # Move the bash profile to the user's directory
-cp -f .bash_profile ~/
+cp -f .zshenv ~/
 
 # Reload the modified bash profile
-. ~/.bash_profile
+. ~/.zshenv
