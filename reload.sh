e2de23e732a44f327ea4fde03b3d5a1779b97ee2
Cory Siebler <cory.siebler@protonmail.com>
2021-04-22 20:42:33 -0700

    Forgot to add cp command for symfony aliases.

--- reload.sh
+++ reload.sh
@@ -3,6 +3,7 @@
 # Move the individual alias files to the user's directory
 cp -f .docker_aliases ~/
 cp -f .git_aliases ~/
+cp -f .symfony_aliases ~/
 cp -f .aliases ~/
 
 # Move the bash profile to the user's directory
